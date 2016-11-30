5abef8902cc2922e1cc6192109fcdc3d16f32625
rhymedys <chr520dys>
2016-11-30 20:25:33 +0800

    update

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -7,32 +7,22 @@
 jQuery(function ($) {
     "use strict";
 
-    var browser = {
-        versions: function () {
-            var u = navigator.userAgent, app = navigator.appVersion;
-            return {//移动终端浏览器版本信息
-                trident: u.indexOf('Trident') > -1, //IE内核
-                presto: u.indexOf('Presto') > -1, //Opera内核
-                webKit: u.indexOf('AppleWebKit') > -1, //苹果/谷歌内核
-                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-                mobile: !!u.match(/AppleWebKit.*Mobile.*/) || !!u.match(/AppleWebKit/), //是否为移动终端
-                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //iOS终端
-                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //Android终端或者UC浏览器
-                iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器
-                iPad: u.indexOf('iPad') > -1, //是否iPad
-                webApp: u.indexOf('Safari') == -1 //是否Web应该程序，没有头部与底部
-            };
-        }(),
-        language: (navigator.browserLanguage || navigator.language).toLowerCase()
-    }
+    var system = {
+        win: false,
+        mac: false,
+        xll: false,
+        ipad: false
+    };
 
-    if (browser.versions.ios || browser.versions.iPhone || browser.versions.iPad) {
-        //window.location="http://localhost/index.html";
-    }
-    else if (browser.versions.android) {
-        //window.location="http://localhost/demo.php";
+    //检测平台
+    var p = navigator.platform;
 
-    } else {
+    system.win = p.indexOf("Win") == 0;
+    system.mac = p.indexOf("Mac") == 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+    system.ipad = (navigator.userAgent.match(/iPad/i) != null) ? true : false;
+
+    if (system.win || system.mac || system.xll||system.ipad) {
         $('.navigation').singlePageNav({
             currentClass: 'active',
             changeHash: true,
@@ -50,10 +40,10 @@ jQuery(function ($) {
             }
         });
 
+
 // prettyphoto
         $("a[data-rel^='prettyPhoto']").prettyPhoto();
-
-    }
+}
 
 
     /* ======= Chart ========= */
